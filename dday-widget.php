94974c4d0198a9f1180ff030ce86541611151abb
Mdkart <Mdkart@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-02-19 17:03:49 +0000

    Now can change easily the title's widget
    git-svn-id: https://plugins.svn.wordpress.org/dday/trunk@32268 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dday-widget.php
+++ dday-widget.php
@@ -3,52 +3,53 @@
 Plugin Name: DDay Widget
 Plugin URI: http://mdkart.fr/blog/2007/04/16/plugin-dday-pour-wordpress
 Description: Adds a Sidebar Widget To Display DDays From DDay Plugin. You Need To Activate DDay First.
-Version: 0.3.2
+Version: 0.3.3
 Author: Mdkart
 Author URI: http://mdkart.fr
+
+Thanks to Xavier Montaron to suggest the modification for title setting.
 */
 
-
-/*  
-	Copyright 2007  Lester Chan  (email : gamerz84@hotmail.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-
-
+define('DDAY_TOKEN', 'DDay');
 ### Function: Init WP-Polls Widget
 function widget_dday_init() {
-	if (!function_exists('register_sidebar_widget')) {
-		return;
-	}
+    if (!function_exists('register_sidebar_widget')) {
+        return;
+    }
 
-	### Function: WP-Polls Widget
-	function widget_dday($args) {
-		extract($args);		
-		if (function_exists('wp_dday_list')) {
-			//$title='Dday (You Can modify this Title will be enhance in next version)'; 
-			echo $before_widget.$before_title.$title.$after_title;
-			wp_dday_list();
-			echo $after_widget;
-		}		
-	}
+    ### Function: WP-Polls Widget
+    function widget_dday($args) {
+        extract($args);        
+        if (function_exists('wp_dday_list')) {
+            $options = get_option('widget_dday');
+            $title = empty($options['title']) ? DDAY_TOKEN : $options['title']; 
+            echo $before_widget; 
+            echo $before_title . $title . $after_title;
+            wp_dday_list();
+            echo $after_widget;
+        }        
+    }
+    
+    function widget_dday_control() {
+		$options = $newoptions = get_option('widget_dday');
+		if ( $_POST["dday-submit"] ) {
+		$newoptions['title'] = strip_tags(stripslashes($_POST["dday-title"]));
+		}
+		if ( $options != $newoptions ) {
+		$options = $newoptions;
+		update_option('widget_dday', $options);
+		}
+		$title = attribute_escape($options['title']);
+        ?> 
+		<p><label for="dday-title"><?php _e('Title:'); ?> <input style="width: 250px;" id="dday-title" name="dday-title" type="text" value="<?php echo $title; ?>" /></label></p>
+		<input type="hidden" id="dday-submit" name="dday-submit" value="1" />
+        <?php 
+    }
 
-	// Register Widgets
-	register_sidebar_widget('DDay', 'widget_dday');
+    // Register Widgets
+    register_sidebar_widget(DDAY_TOKEN, 'widget_dday');
+    register_widget_control(DDAY_TOKEN, 'widget_dday_control');
 }
-
 
 ### Function: Load The WP-Polls Widget
 add_action('plugins_loaded', 'widget_dday_init');
