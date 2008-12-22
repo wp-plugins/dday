db9b28a372c41191b0e1f5df01e30355179e8dc8
Mdkart <Mdkart@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-22 20:07:25 +0000

    correction of an error
    git-svn-id: https://plugins.svn.wordpress.org/dday/trunk@81844 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-dday.php
+++ wp-dday.php
@@ -5,7 +5,7 @@ Plugin URI: http://mdkart.fr/blog/2007/04/16/plugin-dday-pour-wordpress
 Description: This plugin allows you to display DDay's. It also has a spiffy management tool in the administrative console. Fully customizable. 
 Author: Mdkart
 Author URI: http://mdkart.fr
-Version: 0.3.6
+Version: 0.3.5
 Put in /wp-content/plugins/dday/ of your Wordpress installation
 Inpsired by :
 - DDay plugin by Franck Paul for Dotclear : http://franck.paul.free.fr/dotclear/?2005/03/22/105-plugin-jour-j
